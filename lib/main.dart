bdedf18d9e1366be2542f930382dba77f27e1645
Zoir I <wowdobryy@gmail.com>
2022-10-18 16:07:01 +0600

    saving&reading local files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,110 +9,120 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SavingReadingFile(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SavingReadingFile extends StatefulWidget {
+  const SavingReadingFile({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SavingReadingFile> createState() => _SavingReadingFileState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SavingReadingFileState extends State<SavingReadingFile> {
+  final TextEditingController _textController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  static const String kLocalFileName = 'demo_localfile.txt';
+
+  String _localFileContent = '';
+  String _localFilePath = kLocalFileName;
+
+  @override
+  @override
+  void initState() {
+    super.initState();
+    this._readTextFromLocalFile();
+    this
+        ._getLocalFile
+        .then((file) => setState(() => this._localFilePath = file.path));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    FocusNode textFieldFocusNode = FocusNode();
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Local file save/read'),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(20.0),
+        children: [
+          Text('Write to local file:', style: TextStyle(fontSize: 20.0)),
+          TextField(
+            focusNode: textFieldFocusNode,
+            controller: _textController,
+            maxLines: null,
+            style: TextStyle(fontSize: 20.0),
+          ),
+          ButtonBar(
+            children: [
+              MaterialButton(
+                onPressed: () async {
+                  this._readTextFromLocalFile();
+                  this._textController.text = this._localFileContent;
+                  FocusScope.of(context).requestFocus(textFieldFocusNode);
+                  log('String successfully loaded from local file');
+                },
+                child: Text('Load', style: TextStyle(fontSize: 20.0)),
+              ),
+              MaterialButton(
+                onPressed: () async {
+                  await this._writeTextToLocalFile(this._textController.text);
+                  this._textController.clear();
+                  await this._readTextFromLocalFile();
+                  log('String successfully writen lo local file');
+                },
+                child: Text('Save', style: TextStyle(fontSize: 20.0)),
+              ),
+              Divider(height: 20.0),
+              Text('Local file path',
+                  style: Theme.of(context).textTheme.headline6),
+              Text(this._localFilePath,
+                  style: Theme.of(context).textTheme.subtitle1),
+              Divider(height: 20.0),
+              Text('Local file content:',
+                  style: Theme.of(context).textTheme.headline6),
+              Text(this._localFileContent,
+                  style: Theme.of(context).textTheme.subtitle1),
+            ],
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<String> get _getLocalPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> get _getLocalFile async {
+    final path = await _getLocalPath;
+    return File('$path/$kLocalFileName');
+  }
+
+  Future<File> _writeTextToLocalFile(String text) async {
+    final file = await _getLocalFile;
+    return file.writeAsString(text);
+  }
+
+  Future _readTextFromLocalFile() async {
+    String content;
+    try {
+      final file = await _getLocalFile;
+      content = await file.readAsString();
+    } catch (e) {
+      content = 'Error loading local file: $e';
+    }
+    setState(() {
+      this._localFileContent = content;
+    });
   }
 }
